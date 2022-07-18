2bf7334e93817ade70d846def516d8be9fcb2fa8
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2022-07-18 14:22:01 +0100

    Work on creating application file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import gspread
+import logging
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
@@ -9,17 +10,22 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+# For running through terminal
+logging.basicConfig(filename="log.txt", level=logging.DEBUG)
+# For running through py2app
+logging.basicConfig(filename="../../../../log.txt", level=logging.DEBUG)
 
 # Press the green button in the gutter to run the script.
 # if __name__ == '__main__':
 print_hi('PyCharm')
+logging.debug('Hi, PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-gc = gspread.oauth(
-    credentials_filename='credentials.json'
-)
-
-sh = gc.open("TEST")
-
-print(sh.sheet1.cell(1, 1).value)
+# gc = gspread.oauth(
+#     credentials_filename='credentials.json'
+# )
+#
+# sh = gc.open("TEST")
+#
+# print(sh.sheet1.cell(1, 1).value)
